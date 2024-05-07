e49b0c8db4eb7ca726fab22662d7b6b5ec65c9bd
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2024-05-07 16:36:01 -0300

    :sparkles: configuração de rotas com express

--- backend/controller.js
+++ backend/controller.js
@@ -2,7 +2,5 @@
 export function createGraph(req, res){
     const { verifiedQuery } = req.body;
 
-    console.log(verifiedQuery);
-
     return res.status(200).json({ message: "Graph created successfully" });
 }
